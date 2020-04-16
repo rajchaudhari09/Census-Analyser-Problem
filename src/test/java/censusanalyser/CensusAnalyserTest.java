06f13831836037eaf26b92970420ae8469f6dfd8
rajchaudhari09 <rajchaudhari366@gmail.com>
2020-04-16 08:41:49 +0530

    ADDED: The code to check file when correct but delimeter incorrect returne a custome exception

--- src/test/java/censusanalyser/CensusAnalyserTest.java
+++ src/test/java/censusanalyser/CensusAnalyserTest.java
@@ -9,8 +9,9 @@ public class CensusAnalyserTest {
     private static final String INDIA_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
     private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
     private static final String WRONG_CSV_FILE_TYPE = "./src/test/resources/Census.txt";
-    private static final String WRONG_CSV_FILE_DELIMITER = "./src/test/resources/CensusDelimeter.csv";
+    private static final String WRONG_CSV_FILE_DELIMITER1 = "./src/test/resources/CensusDelimeter.csv";
     private static final String WRONG_CSV_FILE_HEADER = "./src/test/resources/CensusInvalidHeader.csv";
+    private static final String WRONG_CSV_FILE_DELIMITER2 = "./src/test/resources/IndiaStateInvalidDelimeter.csv";
     @Test
     public void givenIndianCensusCSVFileReturnsCorrectRecords() {
         try {
@@ -48,7 +49,7 @@ public class CensusAnalyserTest {
     public void givenIndiaCensusData_WithWrongFileDelimiter_ShouldThrowException() {
         try {
             CensusAnalyser censusAnalyser = new CensusAnalyser();
-            censusAnalyser.loadIndiaCensusData(WRONG_CSV_FILE_DELIMITER);
+            censusAnalyser.loadIndiaCensusData(WRONG_CSV_FILE_DELIMITER1);
         } catch (CensusAnalyserException e) {
             Assert.assertEquals(CensusAnalyserException.ExceptionType.INVALID_FILE_TYPE, e.type);
         }
@@ -84,6 +85,16 @@ public class CensusAnalyserTest {
             censusAnalyser.loadIndiaStateCode(WRONG_CSV_FILE_PATH);
         } catch (CensusAnalyserException e) {
             Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM,e.type);
+        }
+    }
+
+    @Test
+    public void givenIndiaStateCode_WithWrongFileDelimiter_ShouldThrowException() {
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            censusAnalyser.loadIndiaStateCode(WRONG_CSV_FILE_DELIMITER2);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.INVALID_FILE_TYPE, e.type);
         }
     }
 }
